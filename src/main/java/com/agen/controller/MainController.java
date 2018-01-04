a07c95b0eb19ae9000d7801dce0d23a3f7edbf37
YoSXD <sxd4business@qq.com>
2018-01-04 16:06:58 +0800

    MainController更新 提交

--- src/main/java/com/agen/controller/MainController.java
+++ src/main/java/com/agen/controller/MainController.java
@@ -3,14 +3,23 @@ package com.agen.controller;
 
 import com.agen.myagen.entity.XxAdmin;
 import com.agen.myagen.repository.AdminRepository;
+import com.agen.orderdiscount.entity.User;
+import com.agen.orderdiscount.repository.UserRepository;
 import com.agen.util.redis.RedisService;
+import com.sxd.util.AngelEncryption;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Objects;
 
 
 @Controller
@@ -20,6 +29,43 @@ public class MainController {
 
     @Resource
     private AdminRepository adminRepository;
+    @Resource
+    private UserRepository userRepository;
+
+
+    @RequestMapping("")
+    public String login(){
+        return "login";
+    }
+
+    /**
+     * 登录 验证
+     * @return
+     */
+    @RequestMapping("loginVer")
+    public String loginVer(String username, String password, Model model, HttpServletRequest request) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+
+        StringBuffer msg = new StringBuffer();
+        User user = userRepository.findUserByUserName(username);
+
+        if(Objects. isNull(user)){
+            msg.append("用户不存在");
+        }else{
+            AngelEncryption angelEncryption = new AngelEncryption();
+            if(user.getPassword().equals(angelEncryption.angelKey(password))){
+                model.addAttribute("msg","success");
+                HttpSession session = request.getSession();
+                session.setAttribute("user",user);
+                return "admin/admin";
+            }else{
+                msg.append("管理密码错误");
+            }
+        }
+        model.addAttribute("msg",msg);
+        return "login";
+    }
+
+
 
     /**
      * 请求地址来源于 基因网
@@ -46,13 +92,22 @@ public class MainController {
         return "index";
     }
 
+    /**
+     * 跳转
+     * @return
+     */
     @RequestMapping("test")
     public String dealTest(){
         return "test";
     }
 
-    @RequestMapping(value = "sub1")
-    public String sub1(){
-        return "/sub/sub1";
+
+    /**
+     * 主页跳转按钮
+     * @return
+     */
+    @RequestMapping("adminHome")
+    public String home(){
+        return "admin/admin";
     }
 }
